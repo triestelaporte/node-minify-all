fa0ed4c120debc3d86ae8419b0ea8fcd9798a968
Trieste LaPorte <trieste@codescience.com>
2017-06-02 16:34:52 -0700

    Updated to understand salesforce static resources

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var minifyAll = function(dir, options, callback){
     options.type = options.type || inputMinType;
 
     walk(dir, function(path, result){
-        if (path.substr(-3) === ".js" || path.substr(-4) === ".css"){
+        if (path.substr(-3) === ".js" || path.substr(-4) === ".css" || path.substr(-9) === ".resource"){
             if (!options.silent){
                 console.log("found file: " + path);
             }
